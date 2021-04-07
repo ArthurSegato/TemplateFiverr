7616fda6e001bc2cd7cb8553b2ca64d92c92cb5f
Arthur Segato Paulo <arthursegato@ctemplar.com>
2021-04-07 08:56:50 -0300

    hehehe

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ document.getElementById("server-ip-highlight").textContent=serverIp;
 function copyText() {
 	var data = [new ClipboardItem({ "text/plain": new Blob([serverIp], { type: "text/plain" }) })];
 	navigator.clipboard.write(data).then();
-	document.getElementById("server-ip-highlight").textContent="I copied!";
+	document.getElementById("server-ip-highlight").textContent="IP copied!";
 	setTimeout(function() { document.getElementById("server-ip-highlight").textContent=serverIp; }, 1000);
 }
 let links = document.getElementsByTagName("a");
